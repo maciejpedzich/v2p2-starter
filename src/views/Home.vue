635e50af4b454233be1297aeedba94226ab49f32
Maciej Pędzich <dnalop.me@gmail.com>
2021-10-03 16:02:36 +0200

    Create home view

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="
+      w-full
+      h-full
+      flex flex-column
+      align-items-center
+      justify-content-center
+    "
+  >
+    <div class="text-6xl font-bold mb-2">V2P2 Starter</div>
+    <div class="text-2xl font-light mb-2">
+      Template for web apps using Vue 3, Vite, and PrimeVue with PWA support.
+      Also featuring:
+    </div>
+    <div class="flex text-lg mt-2" id="featuring">
+      <span>TypeScript</span>
+      <span>Vue Router</span>
+      <span>ESLint</span>
+      <span>Prettier</span>
+      <span>Jest</span>
+      <span>Cypress</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#featuring span {
+  margin-right: 1rem;
+  padding-right: 1rem;
+  border-right: 1px solid var(--surface-500);
+}
+
+#featuring span:last-of-type {
+  border-right: none;
+}
+</style>
