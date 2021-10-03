2ebaa1d229842c23bec0079114b269424833f358
Maciej Pędzich <dnalop.me@gmail.com>
2021-10-03 15:24:49 +0200

    Use PrimeVue and router in Vue app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 
-createApp(App).mount('#app');
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/arya-blue/theme.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(router).use(PrimeVue).mount('#app');
