39dbb9c430beb5cbf6bdb7424a530b6224f98a07
Maciej Pędzich <dnalop.me@gmail.com>
2021-10-03 18:04:19 +0200

    Replace vue-jest with vue3-jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.ts$': 'ts-jest',
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.vue$': 'vue3-jest'
   }
 };
