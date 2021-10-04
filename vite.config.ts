c39632a57222d5e2493ecc38f8f69578aeac58c5
Maciej Pędzich <dnalop.me@gmail.com>
2021-10-04 23:53:56 +0200

    Add and configure Vite PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+
 import eslintPlugin from 'vite-plugin-eslint';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +14,35 @@ export default defineConfig({
       }
     ]
   },
-  plugins: [eslintPlugin(), vue()]
+  plugins: [
+    eslintPlugin(),
+    vue(),
+    VitePWA({
+      base: '/',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'V2P2 Starter',
+        short_name: 'V2P2',
+        theme_color: '#acafb6',
+        icons: [
+          {
+            src: '192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 });
