e3a03bf76c2d91ddd35d9427380ae2c3dde97696
Maciej Pędzich <dnalop.me@gmail.com>
2021-10-23 19:22:28 +0200

    Implement theme switching mechanism

---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import InputSwitch from 'primevue/inputswitch';
+import useThemeSwitch from '../composables/useThemeSwitch';
+
+const { prefersDarkTheme } = useThemeSwitch();
+</script>
+
+<template>
+  <div class="flex align-items-center">
+    <i class="pi pi-sun"></i>
+    <InputSwitch v-model="prefersDarkTheme" class="mx-3" />
+    <i class="pi pi-moon"></i>
+  </div>
+</template>
