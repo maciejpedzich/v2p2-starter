4fadd9722cb127ac0bbc8bb0435dad34e88d6177
Maciej Pędzich <dnalop.me@gmail.com>
2021-10-23 15:21:40 +0200

    Create and register the About page

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,11 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Home',
     path: '/',
     component: () => import('../views/Home.vue')
+  },
+  {
+    name: 'About',
+    path: '/about',
+    component: () => import('../views/About.vue')
   }
 ];
 
