555f0a31270e66209481a1a9021242d142e2918c
Maciej Pędzich <dnalop.me@gmail.com>
2021-10-24 12:14:49 +0200

    Bump version tag

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,8 @@ const currentYear = new Date().getFullYear();
 
 <template>
   <div class="lg:text-6xl text-5xl font-bold mb-2">About</div>
-  <div class="lg:text-lg text-md mb-2">V2P2 Starter v1.1.1</div>
-  <div class="lg:text-lg text-md mb-2">
+  <div class="lg:text-2xl text-xl mb-2">V2P2 Starter v1.1.2</div>
+  <div class="lg:text-2xl text-xl mb-2">
     &copy; 2020-{{ currentYear }} Maciej Pędzich, released under MIT License
   </div>
 </template>
